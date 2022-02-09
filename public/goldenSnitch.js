5811ae18b0686c2c9c3df0d54b6d831d49b33065
Mario Markovic <Mariomarkovic04@outlook.com>
2022-02-09 08:10:38 -0800

    Simple golden snitch chasing game

---
+++ public/goldenSnitch.js
@@ -0,0 +1,82 @@
+class GoldenSnitchGame {
+  constructor(canvas, ctx) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.canvas.width = 1000;
+    this.canvas.height = 1000;
+
+    this.ctx.fillStyle = "lavender";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.input = { up: false, down: false, left: false, right: false };
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "w") this.input.up = true;
+      if (e.key === "s") this.input.down = true;
+      if (e.key === "a") this.input.left = true;
+      if (e.key === "d") this.input.right = true;
+
+      socket.emit("playerInput", this.input);
+    });
+    window.addEventListener("keyup", (e) => {
+      if (e.key === "w") this.input.up = false;
+      if (e.key === "s") this.input.down = false;
+      if (e.key === "a") this.input.left = false;
+      if (e.key === "d") this.input.right = false;
+      socket.emit("playerInput", this.input);
+    });
+  }
+
+  draw(players, snitch) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "lavender";
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.drawPlayers(players);
+    this.drawSnitch(snitch);
+  }
+
+  drawPlayers(players) {
+    for (let player of players) {
+      this.ctx.fillStyle = player.color;
+      this.ctx.fillRect(
+        player.x - player.size / 2,
+        player.y - player.size / 2,
+        player.size,
+        player.size
+      );
+    }
+  }
+
+  drawSnitch(snitch) {
+    this.ctx.fillStyle = "gold";
+    this.ctx.beginPath();
+    this.ctx.arc(snitch.x, snitch.y, snitch.radius, 0, 2 * Math.PI);
+    this.ctx.fill();
+    this.ctx.closePath();
+  }
+
+  handleWin(winner) {
+    this.ctx.fillStyle = "black";
+    this.ctx.font = "20px Serif";
+    this.ctx.textAlign = "center";
+    this.ctx.fillText(
+      `${winner} caught the snitch!`,
+      this.canvas.width / 2,
+      this.canvas.height / 2
+    );
+  }
+}
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const game = new GoldenSnitchGame(canvas, ctx);
+
+const socket = io();
+
+socket.on("gameState", (state) => {
+  game.draw(state.players, state.snitch);
+});
+
+socket.on("gameEnd", (winner) => {
+  game.handleWin(winner);
+});
---
+++ server.js
@@ -0,0 +1,152 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const colors = ["red", "blue", "green", "orange", "purple"];
+let players = [];
+let snitch = {
+  x: 10,
+  y: 10,
+  radius: 10,
+  xVel: 5,
+  yVel: 5,
+  canChangeDir: true,
+};
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  let color = colors.pop();
+  players.push({
+    id: socket.id,
+    x: canvasWidth / 2,
+    y: canvasHeight / 2,
+    size: 25,
+    speed: 4,
+    color: color,
+    input: { up: false, down: false, left: false, right: false },
+  });
+
+  socket.on("playerInput", (input) => {
+    players.find((item) => item.id === socket.id).input = input;
+  });
+
+  socket.on("disconnect", () => {
+    let i = players.findIndex((item) => item.id === socket.id);
+    players.splice(i, 1);
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+const canvasWidth = 1000;
+const canvasHeight = 1000;
+
+const updatePlayers = () => {
+  for (let player of players) {
+    if (player.input.up) player.y -= player.speed;
+    if (player.input.down) player.y += player.speed;
+    if (player.input.left) player.x -= player.speed;
+    if (player.input.right) player.x += player.speed;
+
+    // Put the player in bounds if it ever left
+    if (player.x + player.size / 2 >= canvasWidth)
+      player.x = canvasWidth - player.size / 2;
+    if (player.x - player.size / 2 <= 0) player.x = player.size / 2;
+    if (player.y + player.size / 2 >= canvasHeight)
+      player.y = canvasHeight - player.size / 2;
+    if (player.y - player.size / 2 <= 0) player.y = player.size / 2;
+  }
+};
+
+const startSnitchTimeout = () => {
+  snitch.canChangeDir = false;
+  setTimeout(() => {
+    snitch.canChangeDir = true;
+  }, 250);
+};
+
+const updateSnitch = () => {
+  if (snitch.canChangeDir) {
+    startSnitchTimeout();
+    // Get random velocity for x and y (-8 to 8)
+    snitch.xVel =
+      (Math.round(Math.random()) * 2 - 1) * Math.floor(Math.random() * 1 + 1);
+    snitch.yVel =
+      (Math.round(Math.random()) * 2 - 1) * Math.floor(Math.random() * 1 + 1);
+  }
+
+  // Inverse the velocity if the snitch is running into a wall
+  if (snitch.yVel < 0 && snitch.y - snitch.radius <= 0) snitch.yVel *= -1;
+  if (snitch.xVel < 0 && snitch.x - snitch.radius <= 0) snitch.xVel *= -1;
+  if (snitch.yVel > 0 && snitch.y + snitch.radius >= canvasHeight)
+    snitch.yVel *= -1;
+  if (snitch.xVel > 0 && snitch.x + snitch.radius >= canvasWidth)
+    snitch.xVel *= -1;
+
+  // Inverse the velocity if the snitch is running into the player
+  for (let player of players) {
+    if (
+      Math.sqrt(
+        Math.pow(snitch.x - player.x, 2) + Math.pow(snitch.y - player.y, 2)
+      ) <
+      player.size + snitch.radius + 20
+    ) {
+      if (snitch.y > player.y && snitch.yVel < 0) snitch.yVel *= -1;
+      if (snitch.y < player.y && snitch.yVel > 0) snitch.yVel *= -1;
+      if (snitch.x > player.x && snitch.xVel < 0) snitch.xVel *= -1;
+      if (snitch.x < player.x && snitch.xVel > 0) snitch.xVel *= -1;
+    }
+  }
+
+  snitch.x += snitch.xVel;
+  snitch.y += snitch.yVel;
+
+  // Put the snitch in bounds if it ever left
+  if (snitch.x - snitch.radius < 0) snitch.x = snitch.radius;
+  if (snitch.x + snitch.radius > canvasWidth)
+    snitch.x = canvasWidth - snitch.radius;
+  if (snitch.y - snitch.radius < 0) snitch.y = snitch.radius;
+  if (snitch.y + snitch.radius > canvasHeight)
+    snitch.y = canvasHeight - snitch.radius;
+};
+
+const checkWin = () => {
+  for (let player of players) {
+    if (
+      snitch.x + snitch.radius >= player.x - player.size / 2 &&
+      snitch.x - snitch.radius <= player.x + player.size / 2 &&
+      snitch.y + snitch.radius >= player.y - player.size / 2 &&
+      snitch.y - snitch.radius <= player.y + player.size / 2
+    )
+      return players.find((item) => item.id === player.id).color;
+  }
+
+  return -1;
+};
+
+let tick;
+
+const startGame = () => {
+  tick = setInterval(() => {
+    updatePlayers();
+    updateSnitch();
+    io.emit("gameState", { players: players, snitch: snitch });
+    let winner = checkWin();
+    if (winner != -1) handleWin(winner);
+  }, 10);
+};
+const handleWin = (id) => {
+  console.log(tick);
+  clearInterval(tick);
+  io.emit("gameEnd", id);
+};
+
+startGame();
