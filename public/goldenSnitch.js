cfe6b41f53e56c05da52bed00ac9490af2d118e4
Mario Markovic <Mariomarkovic04@outlook.com>
2022-02-10 20:37:14 -0800

    Refactoring

--- public/goldenSnitch.js
+++ public/goldenSnitch.js
@@ -56,7 +56,7 @@ class GoldenSnitchGame {
   draw(state) {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     // this.drawGrid(state.playerPos);
-    this.drawPlayers(state.nearbyPlayers);
+    // this.drawPlayers(state.nearbyPlayers);
     if (state.relativeSnitch.x && state.relativeSnitch.y)
       this.drawSnitch(state.relativeSnitch);
   }
